a6a6c235539d25d803401d81566ea9d91d915112
Mihail P <mihail.pantuyhin@alseda.by>
2023-07-05 14:22:22 +0300

    add simple notifications

--- app/src/main/java/com/example/socketconnect/MainActivity.kt
+++ app/src/main/java/com/example/socketconnect/MainActivity.kt
@@ -1,8 +1,21 @@
 package com.example.socketconnect
 
+import android.Manifest
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat
 import com.example.socketconnect.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
@@ -14,10 +27,44 @@ class MainActivity : AppCompatActivity() {
     private var binding: ActivityMainBinding? = null
     private val viewModel: MainActivityViewModel by viewModels()
     private var snackBar: Snackbar? = null
+    private var notificationBuilder: NotificationCompat.Builder? = null
+    private var pushCounter = 101
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding?.root)
+        createNotificationChannel()
+        createDefaultNotificationBuilder()
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            checkPushPermission()
+        }
+    }
+
+    fun showNotification(title: String, message: String) {
+        when (PackageManager.PERMISSION_GRANTED) {
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) -> {
+                with(NotificationManagerCompat.from(this)) {
+                    notificationBuilder?.let {
+                        it.setContentTitle(title)
+                        it.setContentText(message)
+                        notify(pushCounter++, it.build())
+                    }
+                }
+            }
+        }
     }
 
     fun showErrorSnackBar(errorMessage: String) {
@@ -25,5 +72,41 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(it, errorMessage, Snackbar.LENGTH_SHORT)
         }
         snackBar?.show()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    private fun checkPushPermission() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            requestPermissionLauncher.launch(
+                Manifest.permission.POST_NOTIFICATIONS
+            )
+        }
+    }
+
+    private fun createDefaultNotificationBuilder() {
+        notificationBuilder = NotificationCompat.Builder(this, "CHANNEL_ID")
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+            .setDefaults(Notification.DEFAULT_SOUND)
+            .setAutoCancel(true)
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.channel_name)
+            val descriptionText = getString(R.string.channel_description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel("CHANNEL_ID", name, importance).apply {
+                description = descriptionText
+            }
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 }
