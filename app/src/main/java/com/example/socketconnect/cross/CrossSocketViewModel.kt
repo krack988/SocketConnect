3caad6f9ec911f857341b9dfc03ea2ef027765d2
Mihail P <mihail.pantuyhin@alseda.by>
2023-07-25 14:35:37 +0300

    remove redundant code, fix crash on connect

--- app/src/main/java/com/example/socketconnect/cross/CrossSocketViewModel.kt
+++ app/src/main/java/com/example/socketconnect/cross/CrossSocketViewModel.kt
@@ -3,14 +3,8 @@ package com.example.socketconnect.cross
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.socketconnect.App
 import com.example.socketconnect.cross.interceptor.AccessInterceptor
 import dagger.hilt.android.lifecycle.HiltViewModel
-import io.ktor.client.HttpClient
-import io.ktor.client.plugins.websocket.WebSockets
-import io.ktor.http.headers
-import io.ktor.http.headersOf
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
@@ -18,15 +12,11 @@ import kotlinx.coroutines.launch
 import okhttp3.OkHttpClient
 import org.hildan.krossbow.stomp.StompClient
 import org.hildan.krossbow.stomp.StompSession
-import org.hildan.krossbow.stomp.config.StompConfig
 import org.hildan.krossbow.stomp.subscribeText
-import org.hildan.krossbow.websocket.ktor.KtorWebSocketClient
 import org.hildan.krossbow.websocket.okhttp.OkHttpWebSocketClient
 import timber.log.Timber
-import java.time.Duration
 import java.util.concurrent.TimeUnit
 import javax.inject.Inject
-import kotlin.time.Duration.Companion.seconds
 
 @HiltViewModel
 class CrossSocketViewModel @Inject constructor() : ViewModel() {
@@ -40,13 +30,13 @@ class CrossSocketViewModel @Inject constructor() : ViewModel() {
 
             val okHttpClient = OkHttpClient.Builder()
                 .addInterceptor(AccessInterceptor())
-                .callTimeout(100000, TimeUnit.MICROSECONDS)
-                .pingInterval(30000, TimeUnit.MICROSECONDS)
+                .callTimeout(100000, TimeUnit.MILLISECONDS)
+                .pingInterval(30000, TimeUnit.MILLISECONDS)
+                .connectTimeout(30000, TimeUnit.MILLISECONDS)
                 .build()
 
-            val httpClient = StompClient(OkHttpWebSocketClient(okHttpClient))
-
             try {
+                val httpClient = StompClient(OkHttpWebSocketClient(okHttpClient))
                 val session: StompSession = httpClient.connect(url = SOCKET_URL)
 
                 val subscription: Flow<String> = session.subscribeText(CHAT_TOPIC)
